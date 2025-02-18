3417cafc78385aa7673423696e52d259a6a0d9d9
Mery Anelo <anelomery@gmail.com>
2025-02-18 18:22:19 +0100

    Try to use default browse lng config

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,12 +4,18 @@ import { initReactI18next } from "react-i18next";
 import en from "./locales/en.json";
 import es from "./locales/es.json";
 
+const supportedLanguages = ["en", "es"];
+
+const browserLanguage = navigator.language.split("-")[0];
+
+const defaultLanguage = supportedLanguages.includes(browserLanguage) ? browserLanguage : "en";
+
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
     es: { translation: es },
   },
-  lng: "en",
+  lng: defaultLanguage,
   fallbackLng: "en",
   interpolation: {
     escapeValue: false,
